59aabcff53a0fa8bc1d67d6f7b42a81c3a5b1883
Lucas Mazzutti <lucasmazzutti@MacBook-Pro-de-Lucas.local>
2020-02-17 23:17:48 +0100

    Building MVC

---
+++ 9-forkify/starter/src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults() {
+        try{
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        }catch (error){
+            alert(error);
+        }
+    }
+}
