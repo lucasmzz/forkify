dfa28ca851ed6ad417075c7d33c9b92b22639a70
Lucas Mazzutti <lucasmazzutti@MacBook-Pro-de-Lucas.local>
2020-02-22 16:30:45 +0100

    performing search

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,8 +1,32 @@
 import Search from './models/Search';
+import * as SearchView from './views/SearchView';
+import { elements } from './views/base';
 
+const state = {};
+
+const controlSearch = async () => {
+
+    const query = SearchView.getInput();
+
+    if (query){
+        //new Search Model instance stored in app state
+        state.search = new Search(query);
+
+        //Prepare UI for results
+        SearchView.clearInput();
+        SearchView.clearResults();
+        
+        //fetch results into app state
+        await state.search.getResults();
+        
+        //Render results on UI
+        SearchView.renderResults(state.search.result);
+    }
+}
 const search = new Search('pizza');
 
-document.querySelector('.search__btn').addEventListener('click',(e) => {
+elements.searchForm.addEventListener('submit',e => {
     e.preventDefault();
+    controlSearch();
     search.getResults();
 });
--- starter/src/js/models/Search.js
+++ starter/src/js/models/Search.js
@@ -10,7 +10,6 @@ export default class Search {
             const proxy = 'https://cors-anywhere.herokuapp.com/';
             const res = await axios(`${proxy}https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log(this.result);
         }catch (error){
             alert(error);
         }
